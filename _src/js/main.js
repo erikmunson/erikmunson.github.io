15e6e94987756ac067857f2ed3df3e5293e6afcc
erikmunson <erik@ipsumcreative.com>
2014-04-28 21:37:07 -0400

    Disable lettering.js.

--- _src/js/main.js
+++ _src/js/main.js
@@ -40,12 +40,12 @@ var fixWidows = function() {
 
 var initialize = function() {
 
-	var h3_tags = document.getElementsByTagName("h3");
-
-	for (var i = 0; i < h3_tags.length; i++) {
-
-		lettering.splitter(h3_tags[i]);
-	};
+	// var h3_tags = document.getElementsByTagName("h3");
+	//
+	// for (var i = 0; i < h3_tags.length; i++) {
+	//
+	// 	lettering.splitter(h3_tags[i]);
+	// };
 
 	fixWidows();
 
